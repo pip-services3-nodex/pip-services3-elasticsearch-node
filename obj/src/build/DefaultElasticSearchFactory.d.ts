8172227489041c7ebd8f62fec458c7aa7a86f0b1
Sergey Seroukhov <seroukhov@gmail.com>
2021-05-28 20:23:15 -0700

    Initial commit

---
+++ obj/src/build/DefaultElasticSearchFactory.d.ts
@@ -0,0 +1,14 @@
+/** @module build */
+import { Factory } from 'pip-services3-components-nodex';
+/**
+ * Creates ElasticSearch components by their descriptors.
+ *
+ * @see [[ElasticSearchLogger]]
+ */
+export declare class DefaultElasticSearchFactory extends Factory {
+    private static readonly ElasticSearchLoggerDescriptor;
+    /**
+     * Create a new instance of the factory.
+     */
+    constructor();
+}
